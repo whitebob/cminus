2394fa76e2737affed023d409f95a6ba498a0288
whitebob <qliu@semi.ac.cn>
2018-12-20 20:04:54 +0800

    Add: save load functions. Adjusst: prevent reload of $PROMPT_COMMAND BugFix: tab complete under -f

--- cminus.sh
+++ cminus.sh
@@ -1,4 +1,4 @@
-PROMPT_COMMAND="$PROMPT_COMMAND"$';c-_pushd'
+type c- &> /dev/null || PROMPT_COMMAND="$PROMPT_COMMAND"$';c-_pushd'
 
 c-_pushd() { 
         eval "case $( md5 -q -s "`pwd`") in ' ' $( dirs -l | tail -n +2 | cut -d\  -f 4- |sed -e 's:^:":g'  -e 's:$:":g' | xargs -n1 md5 -q -s | sed -e 's:^: | :g' | xargs ) ) ;; * ) pushd . > /dev/null;; esac"; 
@@ -10,16 +10,22 @@ c-_completion() {
         cur="${COMP_WORDS[COMP_CWORD]}"
         pre="${COMP_WORDS[COMP_CWORD-1]}"
         case ${pre} in
-                "-f"|"--fuzzy" ) COMPREPLY=( $( compgen -W "$( echo ${DIRSTACK[@]} | xargs -n1| tail -n +2 |grep ${cur}| xargs)" -- ) );; #fuzzy
+                "-s"|"-l"|"--save"|"--load" ) COMPREPLY=( $( compgen -f -- ${cur} ));; # save/load
+                "-f"|"--fuzzy" ) [ -z ${cur} ] || COMPREPLY=( $( compgen -W "$( echo ${DIRSTACK[@]} | xargs -n1| tail -n +2 |grep ${cur}| xargs)" -- ) );; #fuzzy
                 * ) COMPREPLY=( $( compgen -W "$( echo ${DIRSTACK[@]} | xargs -n1| tail -n +2 | xargs)" --  ${cur} ) );; #traditional
         esac
         return 0;
 }
 
 c-() {
-        case $1 in "-f" ) shift;; esac
-        cd $*;
-        return $?;
+        local path
+        case $1 in 
+                "-f" ) shift;; 
+                "-s" ) echo ${DIRSTACK[@]} | xargs -n1 | tail -n +2 > $2; return $?;; 
+                "-l" ) while read path; do pushd -n ${path}; done < $2; return $?;; 
+        esac
+        cd $*
+        return $?
 }
 
 complete -o bashdefault -F c-_completion c-
