cfd8e8808e89f9203b6e3fde1c97a2dfe0836b39
whitebob <qliu@semi.ac.cn>
2018-12-21 15:01:08 +0800

    Add: OLDPWD push/pop to make cd - work.

--- cminus.sh
+++ cminus.sh
@@ -1,12 +1,15 @@
 type c- &> /dev/null || PROMPT_COMMAND="$PROMPT_COMMAND"$';c-_pushd'
 
-c-_pushd() { 
-        eval "
+c-_pushd() {
+        local pushpwd
+        pushpwd=${OLDPWD}
+        eval "  
                 case $( md5 -q -s "`pwd`") in 
                         ' ' $( dirs -l | tail -n +2 | cut -d\  -f 4- | sed -e 's:^:":g'  -e 's:$:":g' | xargs -n1 md5 -q -s | sed -e 's:^: | :g' | xargs ) ) ;; 
                         * ) pushd . > /dev/null;; 
                 esac
-        "; 
+        ";
+        OLDPWD=${pushpwd} # recover OLDPWD to make "cd -" work as before. 
 }
 
 c-_completion() {
@@ -16,7 +19,7 @@ c-_completion() {
         pre="${COMP_WORDS[COMP_CWORD-1]}"
         case ${pre} in
                 "-s"|"-l"|"--save"|"--load" ) COMPREPLY=( $( compgen -f -- ${cur} ));; # use file complete for save/load
-                "-f"|"--fuzzy" ) [ -z ${cur} ] || COMPREPLY=( $( compgen -W "$( echo ${DIRSTACK[@]} | xargs -n1 | tail -n +2 | grep ${cur}| xargs)" -- ) ) && 
+                "-f"|"--fuzzy" ) [ -z ${cur} ] || COMPREPLY=( $( compgen -W "$( echo ${DIRSTACK[@]} | xargs -n1 | tail -n +2 | egrep ${cur} | xargs)" -- ) ) && 
                         if (( ${#COMPREPLY[@]} >= 2 )); then # show the candidates
                                 echo
                                 echo ${COMPREPLY[@]} | xargs -n1 | column -c ${COLUMNS}
