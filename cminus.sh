a460b92a220106b83507bc8b65245ed49c02a6ca
whitebob <qliu@semi.ac.cn>
2018-12-24 23:08:41 +0800

    Add: License lines and author info.

--- cminus.sh
+++ cminus.sh
@@ -1,3 +1,19 @@
+#   c- (cminus) is a bash script help you quickly go back to dirs visited. 
+#   Copyright (C) 2018 Qi Liu <gwhitebob@gmail.com> 
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 type c- &> /dev/null || PROMPT_COMMAND="$PROMPT_COMMAND"$';c-_pushd'
 
 c-_pushd() {
