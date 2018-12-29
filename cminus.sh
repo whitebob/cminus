9ec70d8dc258d4810b70d6357adf6c669bba4a97
whitebob <qliu@semi.ac.cn>
2018-12-30 01:09:31 +0800

    Add: help option, inline example. Bugfix: make default completion output more info to feel better.

--- cminus.sh
+++ cminus.sh
@@ -34,27 +34,31 @@ c-_completion() {
         cur="${COMP_WORDS[COMP_CWORD]}"
         pre="${COMP_WORDS[COMP_CWORD-1]}"
         case ${pre} in
-                "-f"|"--fuzzy" ) [ -z ${cur} ] || eval COMPREPLY=( $( compgen -W "$( for path in "${DIRSTACK[@]}"; do echo ${path}; done | tail -n +2 | sort | uniq | egrep ${cur} | sed -e "s:^:':g" -e s":$:':g" | while read match; do echo -n "$match "; done )" -- | sed -e "s:^:':g" -e "s:$:':g" ) ) && 
-                        if (( ${#COMPREPLY[@]} >= 2 )); then # show the candidates
-                                echo; for match in  "${COMPREPLY[@]}"; do echo ${match}; done |egrep --color=always ${cur} | column -c ${COLUMNS};echo -e "\033[01;32m${#COMPREPLY[@]}\033[00m records matched."; echo -n ${COMP_WORDS[@]};
-                        fi 
-                ;; # fuzzy match complete
                 "-s"|"-l"|"--save"|"--load" ) COMPREPLY=( $( compgen -f -- ${cur} ) );; # use file complete for save/load
                 "-r"|"--refresh" ) ;; # refresh to remove duplicate items due to load or manual pushd operations
-                * ) eval COMPREPLY=( $( compgen -W "$( for path in "${DIRSTACK[@]}"; do echo \'${path}\'; done | tail -n +2 | sort | uniq )" --  ${cur} | sed -e "s:^:':g" -e "s:$:':g" ) ); (( ${#COMPREPLY[@]} > 1 )) && echo -e "\n\033[01;32m${#COMPREPLY[@]}\033[00m records matched." && echo -n ${COMP_WORDS[@]};; # traditional complete style
+                "-f"|"--fuzzy" ) eval COMPREPLY=( $( compgen -W "$( for path in "${DIRSTACK[@]}"; do echo ${path}; done | tail -n +2 | sort | uniq | if [ -z ${cur} ]; then cat; else grep -e "${cur}";fi | sed -e "s:^:':g" -e s":$:':g" | while read match; do echo -n "$match "; done )" -- | sed -e "s:^:':g" -e "s:$:':g" ) );; # fuzzy match complete
+               * ) eval COMPREPLY=( $( compgen -W "$( for path in "${DIRSTACK[@]}"; do echo \'${path}\'; done | tail -n +2 | sort | uniq )" --  ${cur} | sed -e "s:^:':g" -e "s:$:':g" ) );; # traditional complete style
         esac
+        if (( ${#COMPREPLY[@]} > 1 )); then # show the candidates
+                echo; for match in  "${COMPREPLY[@]}"; do echo ${match}; done | if [ -z ${cur} ]; then cat; else grep --color=always -e "${cur}"; fi | sort | uniq | column -c ${COLUMNS}; echo -e "\033[01;32m${#COMPREPLY[@]}\033[00m records matched." && echo -n ${COMP_WORDS[@]}
+        fi
         return 0;
 }
 c-() {
         if [ ! -z ${CMINUSDISABLED} ]; then return 0; fi
         local path unistack
         case $1 in 
-                "-f"|"--fuzzy" ) shift;; 
-                "-s"|"--save" ) for path in "${DIRSTACK[@]}"; do echo ${path}; done | tail -n +2 > $2; return $?;; 
+                "-f"|"--fuzzy" ) shift;;
+                "-h"|"--help" ) shift;;
+                "-s"|"--save" ) for path in "${DIRSTACK[@]}"; do echo ${path}; done | tail -n +2 > $2; return $?;;
                 "-l"|"--load" ) while read path; do [ -z $( echo ${path} | egrep ${CMINUSIGNORE} ) ] && pushd -n "${path}"; done < $2 >/dev/null; CMINUSHASH="' '$( for path in "${DIRSTACK[@]}"; do echo "${path}"; done | tail -n +2 | while read path; do eval ${CMINUSMD5PROG} \"${path}\"; done | cut -c-7 | sed -e 's:^:|:g' | sort | uniq | xargs -n1 echo -n )"; return $?;;
                 "-r"|"--refresh" ) unistack=(); eval unistack=( $( for path in "${DIRSTACK[@]}"; do echo \'${path}\'; done | tail -n +2 | sort | uniq ) ); dirs -c; for path in "${unistack[@]}"; do pushd -n "${path}"; done > /dev/null; CMINUSHASH="' '$( for path in "${DIRSTACK[@]}"; do echo "${path}"; done | tail -n +2 |while read path; do eval ${CMINUSMD5PROG} \"${path}\"; done | cut -c-7 | sed -e 's:^:|:g' | sort | uniq | xargs -n1 echo -n )"; return $?;;
         esac
-        cd "$*" # quote is necessary when spaces in path
+        if [ -z $* ]; then
+                echo -e "Usage: \nc- -f|--fuzzy re_expr\t(fuzzy search)\nc- -s|--save filename\t(save paths)\nc- -l|--load filename\t(load paths)\nc- -r|--refresh\t\t(refresh paths)\nc- -h|--help\t\t(this info)\nExample 1:\033[01;32m c- -f m.*n$ \033[00m to find path with re_format \"m.*n$\".\nExample 2:\033[01;32m c- -l <(find \`pwd\` -type d)\033[00m to load paths in current dir.\nExample 3:\033[01;32m c- -r \033[00m to clean duplicated dirs in stack.\nHit <Tab> after \"c- \"or \"c- -f \" to auto-complete the path.\nDon't forget the tailing <Space>."
+        else
+                cd "$*"; # quote is necessary when spaces in path
+        fi
         return $?
 }
 complete -o bashdefault -F c-_completion c-
